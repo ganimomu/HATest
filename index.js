981467116496b96a62c965db5c1717446ab5b947
Gabriel Moreno <ganimomu2001@gmail.com>
2021-06-29 19:18:52 -0300

    new update

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+require("dotenv").config();
+const path = require("path");
 const express = require("express");
+const routes = require("./routes");
 const app = express();
-app.get('/ejemplo', (req, res) => {​
-    res.end('Llegaste ejemplo');
-}​);
-app.get('/usuarios', (req, res) => {​
-    res.end('Llegaste usuarios');
-}​);
-app.listen(3000, () => console.log(`Servidor corriendo en el puerto!`));
+const nunjucks = require("nunjucks");
+const { sequelize } = require("./models");
+const PORT = process.env.APP_PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+app.set("view engine", "njk");
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(routes);
+
+// sequelize.sync({ force: true }).then(async () => {
+//   await require("./seeders")();
+// });
+
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}!`));
