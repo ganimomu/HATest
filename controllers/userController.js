a84e746ba79405992c6c1e3a00cbab9449dc1e5c
Gabriel Moreno <ganimomu2001@gmail.com>
2021-07-01 19:47:01 -0300

    lol

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const path = require("path");
 const formidable = require("formidable");
 const { User } = require("../models");
 const { response } = require("express");
+const AWS = require('aws-sdk');
 
 module.exports = {
   index: async (req, res) => {
@@ -10,7 +11,8 @@ module.exports = {
     res.render("users", { users: usuarios });
   },
 
-  store: async (req, res) => {    
+  storeLocal: async (req, res) => {    
+    
     const form = formidable({
       multiples: true,
       uploadDir: __dirname + "/../public/img",
@@ -30,6 +32,52 @@ module.exports = {
         firstname: fields.firstname,
         lastname: fields.lastname,
         avatar: path.basename(files.avatar.path),
+      });
+
+      res.redirect('/usuarios');
+    
+    });
+  },
+
+  storeInS3: async (req, res) => {    
+    
+    const form = formidable({
+      multiples: true,
+      keepExtensions: true,
+    });
+    
+    form.parse(req, async (err, fields, files) => {
+
+      // Configuración del SDK de Amazon para conectarnos a nuestra cuenta
+      AWS.config.update({
+        region: process.env.AWS_S3_BUCKET_REGION
+      });
+
+      // Creo una instancia de S3 para conectarme con el bucket
+      const s3 = new AWS.S3({
+        apiVersion: process.env.AWS_S3_API_VERSION,
+        accessKeyId: process.env.AWS_USER_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_USER_SECRET_ACCESS_KEY,
+      });
+
+      // Obtener extension y nombre del archivo para subir
+      const ext = path.extname(files.avatar.path);
+      const filename = `image_${Date.now()}${ext}`;
+      // Lo mismo que: const filename = 'image_' + Date.now() + ext;
+
+      // Subir archivo a S3
+      await s3.upload({
+        ACL: 'public-read',
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: `avatars/${filename}`,
+        ContentType: files.avatar.type,
+        Body: fs.createReadStream(files.avatar.path),
+      }).promise();
+
+      await User.create({      
+        firstname: fields.firstname,
+        lastname: fields.lastname,
+        avatar: filename,
       });
 
       res.redirect('/usuarios');
